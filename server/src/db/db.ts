c940c57bb6043d4494e79d2eba1586f38f48f7d3
Rispectech <ue208085.rishabhverma.it@gmail.com>
2022-01-25 21:46:28 +0530

    removed password

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 const ConnectServerString =
-  "mongodb+srv://Rishabh:123@nodeexpressprojects.yil9q.mongodb.net/3-Task-Manager?retryWrites=true&w=majority";
+  "mongodb+srv://Rishabh:<password></password>@nodeexpressprojects.yil9q.mongodb.net/3-Task-Manager?retryWrites=true&w=majority";
 
 mongoose.connect(ConnectServerString);
